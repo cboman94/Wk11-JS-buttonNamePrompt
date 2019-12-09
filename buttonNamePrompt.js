98c308d43683f09ffe62abfd736192836950e993
unknown <101_04@sest.local>
2019-12-09 09:51:27 -0500

    altered files

--- buttonNamePrompt.js
+++ buttonNamePrompt.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('button');
 
 button.onclick = function() {
-  let name = prompt('What is your name?');
-  alert('Hello ' + name + ', nice to see you!');
+  let name = prompt('how big your head?');
+  alert('your head is this big ' + name + ', you is smort!');
 }
